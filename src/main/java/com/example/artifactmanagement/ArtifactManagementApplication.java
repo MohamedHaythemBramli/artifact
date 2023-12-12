dcaa7f0ba821002390794778c14fe32e7083ee12
MohamedHaythemBramli <mohamedhaythembramli90@gmail.com>
2023-12-12 14:23:43 +0100

    First Commit

---
+++ src/main/java/com/example/artifactmanagement/ArtifactManagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.artifactmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtifactManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArtifactManagementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/artifactmanagement/ArtifactManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.artifactmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtifactManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
